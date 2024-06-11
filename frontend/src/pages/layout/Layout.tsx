985d02e1bc489c16628fbf2ef0dc08c99727a82b
Joe Crowley <mistaacrowley@gmail.com>
2024-06-11 09:44:26 -0700

    add href

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -71,10 +71,19 @@ const Layout = () => {
       <header className={styles.header} role={'banner'}>
         <Stack horizontal verticalAlign="center" horizontalAlign="space-between">
           <Stack horizontal verticalAlign="center">
-            <img src={ui?.logo ? ui.logo : Contoso} className={styles.headerIcon} aria-hidden="true" alt="" />
-            <Link to="/" className={styles.headerTitleContainer}>
-              <h1 className={styles.headerTitle}>{ui?.title}</h1>
-            </Link>
+            <a href="https://fattailed.ai" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+              <img
+                src={ui?.logo ? ui.logo : Contoso}
+                className={styles.headerIcon}
+                aria-hidden="true"
+                alt=""
+              />
+            </a>
+            <a href="https://fattailed.ai" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+              <div className={styles.headerTitleContainer}>
+                <h1 className={styles.headerTitle}>{ui?.title}</h1>
+              </div>
+            </a>
           </Stack>
           <Stack horizontal tokens={{ childrenGap: 4 }} className={styles.shareButtonContainer}>
             {appStateContext?.state.isCosmosDBAvailable?.status !== CosmosDBStatus.NotConfigured && (
