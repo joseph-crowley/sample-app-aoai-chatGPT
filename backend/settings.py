5b41d28b3a8d950d10c9b2cbcef5e0d995d4e652
Joe Crowley <mistaacrowley@gmail.com>
2024-06-11 01:40:14 -0700

    update prompts

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Fat Tailed AI"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "How May I Help You?"
+    chat_description: str = "This chatbot is configured to answer questions about Julia and Danny's Wedding."
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
 
